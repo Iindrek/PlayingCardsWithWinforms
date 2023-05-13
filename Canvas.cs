8e6465949693fc4162b56837bd424d23e3a1efd4
Inx <Inx@DESKTOP-MKKRFCR>
2023-05-13 08:20:45 +0300

    Add project files.

---
+++ Canvas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayingCardsWithWinforms
+{
+    public partial class Form1 : Form
+    {
+
+        List<PictureBox> cards = new List<PictureBox>();
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void createCardsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 36; i++)
+            {
+                PictureBox card = new PictureBox();
+                card.Width = 100;
+                card.Height = 200;
+                card.BackColor = Color.SteelBlue;
+                card.Top = rand.Next(400);
+                card.Left = rand.Next(800);
+                cards.Add(card);
+                this.Controls.Add(card);
+            }
+
+            
+        }
+    }
+}
